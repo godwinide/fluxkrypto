80163651377b8008403df95f7988f4c3a9885e44
godwinide <somedev@gmail.com>
2025-06-08 12:30:58 -0700

    first commit

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../model/User');
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      // Match user
+      User.findOne({ email: { $regex: email, $options: 'i' } })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'invalid email or password' });
+          }
+          // Match password
+          bcrypt.compare(password.trim(), user.password.trim(), (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'invalid email or password' });
+            }
+          });
+        });
+    })
+  );
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+};
